e9c7082b68b9e39128d6d14baf01ba9c6616f2da
Minde Vieras <mindevieras@gmail.com>
2019-01-03 20:07:15 +0000

    menu click scroll

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ function requireAll(r) { r.keys().forEach(r) }
 requireAll(require.context('../images/', true))
 
 import WebFont from 'webfontloader'
+var scrollToElement = require('scroll-to-element')
 
 // Import styles
 import '../sass/main.scss'
@@ -19,10 +20,38 @@ WebFont.load({
   }
 });
 
+// Set initial variables
+const mainWrapper = document.getElementById('main_wrapper')
+const menuLinks = document.querySelectorAll('#main_navigation .menu-link')
+const menuArray = [...menuLinks]
+
 function init() {
 
-  const mainWrapper = document.getElementById('main_wrapper')
+  // Munu links clicks
+  menuArray.map(link => {
+    link.addEventListener('click', menuClick)
+  })
 
   // Set #main_wrapper opacity to 1 when js loaded
   mainWrapper.style.opacity = 1
+}
+
+// Menu click function
+function menuClick(e) {
+
+  e.preventDefault()
+
+  // remove active class
+  menuArray.map(link => link.classList.remove('active'))
+  // add active class
+  this.classList.add('active')
+  
+  // Scroll to section
+  const { section } = this.dataset
+  if(section) {
+    scrollToElement(`#${section}`, {
+      offset: 0,
+      duration: 750
+    });
+  }
 }
